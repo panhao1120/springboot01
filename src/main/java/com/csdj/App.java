6ba6bf15fe630dec855af63341ebbcebf16238b9
PanHao <1687934235@qq.com>
2022-09-01 09:19:57 +0800

    springboot 第一次提交

---
+++ src/main/java/com/csdj/App.java
@@ -0,0 +1,13 @@
+package com.csdj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
